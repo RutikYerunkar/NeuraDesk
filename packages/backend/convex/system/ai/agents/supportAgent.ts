c33906ea16104f3522a4109db7c83755f7b355df
Rutik Yerunkar <rutikyerunkar@gmail.com>
2025-09-02 21:48:19 -0700

    19: ai tool calling

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -4,5 +4,5 @@ import { components } from "../../../_generated/api";
 
 export const supportAgent = new Agent(components.agent, {
   chat: openai.chat("chatgpt-4o-latest"),
-  instructions: "You are a customer support agent",
+  instructions: `You are a customer support agent. Use "resolveConversation" tool when user expresses finalization of the conversation. Use "escalateConversation" tool when user expresses frustration, or requests a human explicitly.`,
 });
